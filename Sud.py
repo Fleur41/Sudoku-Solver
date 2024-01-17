daeebbaed0b1555a53820137993c53066d586db7
Fleur41 <Muriithikings456@gmail.com>
2024-01-17 12:27:46 +0300

    Initial commit

---
+++ Sud.py
@@ -0,0 +1,7 @@
+'''
+Sudoku Solver
+-------------------------------------------------------------
+pip install pygame
+image link:
+https://www.pngitem.com/pimgs/m/210-2106648_empty-sudoku-grid-grid-6x6-png-transparent-png.png
+'''
