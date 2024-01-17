f33fa892a2c6a54235e847c3384a7176bdc387e5
Fleur41 <Muriithikings456@gmail.com>
2024-01-17 13:01:30 +0300

    solve

--- Sud.py
+++ Sud.py
@@ -76,3 +76,20 @@ def raise_error_2():
     screen.blit(text1, (20, 570))
 
 def valid(m, i, j, val):
+    for it in range(9):
+        if m[i][it] == val:
+            return False
+        if m[it][j] == val:
+            return False
+        
+
+    it = i // 3
+    jt = j // 3
+
+    for i in range(it * 3, it * 3 + 3):
+        for j in range(jt * 3, jt * 3 + 3):
+            if m[i][j] == val:
+                return False
+    return True
+
+def solve(grid, i, j):
